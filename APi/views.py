63b07fd4a1699ba5cc8a9385f2238050b8c5cf14
Anuj <anuj892002@gmail.com>
2025-12-28 22:19:20 +0530

    Class Based - Parameters
    Signed-off-by: Anuj <anuj892002@gmail.com>

--- APi/views.py
+++ APi/views.py
@@ -65,9 +65,6 @@ class Employees(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-
-          
-
-
-        
+  
+class EmployeeDetails(APIView):
+    
